102b93c6ba1d2d3ada945044aceaf6b886f9c1d2
danalee <eleeje97@gmail.com>
2023-01-20 11:10:07 +0900

    project init

---
+++ todo/src/test/java/com/whooper/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whooper.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
